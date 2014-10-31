92465447a331ebf9f530e18c2ab0802468a09c18
Ngoc Dao <ngocdaothanh@gmail.com>
2014-11-01 07:32:21 +0900

    Add sinetja.mode

--- script/start.bat
+++ script/start.bat
@@ -20,7 +20,7 @@ for /f "delims=. tokens=1-3" %%v in ("%JAVA_VERSION%") do (
 # You may need to customize memory config below to optimize for your environment.
 # To display time when the application is stopped for GC:
 # -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime
-set JAVA_OPTS=-Xmx1024m -Xms256m %MEM_OPTS% -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -Djava.awt.headless=true -server -Dxitrum.mode=production
+set JAVA_OPTS=-Xmx1024m -Xms256m %MEM_OPTS% -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -Djava.awt.headless=true -server -Dsinetja.mode=production
 
 set ROOT_DIR=%~dp0..
 cd "%$ROOT_DIR%"
