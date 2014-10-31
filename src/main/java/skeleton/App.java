04f7e9bbdbad76881f3d77edea9c042609e28f69
Ngoc Dao <ngocdaothanh@gmail.com>
2014-10-31 19:03:43 +0900

    Remove Java 8 lambda syntax

--- src/main/java/skeleton/App.java
+++ src/main/java/skeleton/App.java
@@ -1,18 +1,25 @@
 package skeleton;
 
 import sinetja.Server;
+import sinetja.Action;
+import sinetja.Request;
+import sinetja.Response;
 
 public class App {
   public static void main(String[] args) {
     new Server()
 
-    .GET("/", (req, res) -> {
-      res.respondText("Hello world");
+    .GET("/", new Action() {
+      public void run(Request req, Response res) throws Exception {
+        res.respondText("Hello world");
+      }
     })
 
-    .GET("/hello/:name", (req, res) -> {
-      String name = req.param("name");
-      res.respondText("Hello " + name);
+    .GET("/hello/:name", new Action() {
+      public void run(Request req, Response res) throws Exception {
+        String name = req.param("name");
+        res.respondText("Hello " + name);
+      }
     })
 
     .start(8000);
